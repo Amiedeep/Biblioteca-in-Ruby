1d0ec9b8b8f5e9224905ef6b3ac1eb5231a7f2c7
Amandeep Singh <amandeepsingh@INAmandees.local>
2015-11-24 18:51:04 +0530

    [Release 1, List Books] added functionality to print books on the screen after user start application

--- spec/biblioteca_application_spec.rb
+++ spec/biblioteca_application_spec.rb
@@ -3,14 +3,33 @@ require './src/biblioteca_application';
 describe 'BibliotecaApplication' do
 
   before(:each) {
-    @biblioteca_application = BibliotecaApplication.new
+    @biblioteca_application = BibliotecaApplication.new('Welcome to biblioteca')
+  }
+
+  before(:all) {
+    @original_io = $stdout
+    $stdout = StringIO.new
+  }
+
+  after(:all) {
+    $stdout = @original_io
   }
 
   describe '#start_application' do
 
     it 'should print welcome message on console' do
-      expect(STDOUT).to receive(:puts).with('Welcome to biblioteca')
       @biblioteca_application.start_application
+      expect(console_output).to include('Welcome to biblioteca')
+    end
+
+    it 'should print books' do
+      @biblioteca_application.start_application
+      expect(console_output).to include("Available books are : \nthe pragmattic programmer\nclean code")
     end
   end
+end
+
+private
+def console_output
+  $stdout.string
 end
--- src/biblioteca_application.rb
+++ src/biblioteca_application.rb
@@ -1,12 +1,23 @@
 class BibliotecaApplication
 
+  def initialize(welcome_message = "Welcome to biblioteca")
+    @welcome_message = welcome_message
+    @books = ['the pragmattic programmer', 'clean code']
+  end
+
   def start_application
-    welcome_message
+    print_welcome_message
+    print_books
   end
 
   private
-  def welcome_message
-    puts "Welcome to biblioteca"
+  def print_welcome_message
+    puts @welcome_message
+  end
+
+  def print_books
+    puts "Available books are : \n"
+    puts @books
   end
 
   BibliotecaApplication.new.start_application
