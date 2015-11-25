8c91868ef11953822339497982c16eafa74737cc
Amandeep Singh <Amandees@thoughtworks.com>
2015-11-25 16:42:00 +0530

    [Release 1, Main Menu] Added Library class to accept books array and a method to print them in formatted order

---
+++ spec/library_spec.rb
@@ -0,0 +1,19 @@
+require './src/library'
+
+describe "Library" do
+  
+  describe "#list_books" do
+
+    it 'should print headers and list all the books inside library' do
+      book1 = double("Book")
+      book2 = double("Book")
+      library = Library.new([book1, book2])
+      expected_headers = "Available books are : \n" + "%-50s %-50s" % ["Name", "Author"] + "Year of Publication"
+
+      allow(book1).to receive("to_str").and_return "book1"
+      allow(book2).to receive("to_str").and_return "book2"
+
+      expect(library.list_books).to eq(expected_headers + "book1book2")
+    end
+  end
+end
---
+++ src/library.rb
@@ -0,0 +1,15 @@
+class Library
+
+  def initialize available_books
+    @available_books = available_books
+  end
+
+  def list_books
+    print_headers + @available_books.each{ |book| book.to_str }.join
+  end
+
+  private
+  def print_headers
+    "Available books are : \n" + "%-50s %-50s" % ["Name", "Author"] + "Year of Publication"
+  end
+end
