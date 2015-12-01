7890a184fc917d30f5d2aca94e696200a0e8b787
Amandeep Singh <Amandees@thoughtworks.com>
2015-12-01 16:42:49 +0530

    [Release 1, Quit] Adding quit option on selection of q in menu_options

--- spec/menu_options_spec.rb
+++ spec/menu_options_spec.rb
@@ -7,7 +7,7 @@ describe "MenuOptions" do
     it 'should return the menu option' do
       menu_options = MenuOptions.new
 
-      expect(menu_options.get_options).to eq("Please select a option\n1: List Books\n")
+      expect(menu_options.get_options).to eq("Please select a option\n1: List Books\nq: Quit\n")
     end
   end
 end
--- src/menu_options.rb
+++ src/menu_options.rb
@@ -5,6 +5,6 @@ class MenuOptions
   end
 
   def get_options
-      "Please select a option\n1: " + @menu_option + "\n"
+      "Please select a option\n1: " + @menu_option + "\nq: Quit\n"
   end
 end
