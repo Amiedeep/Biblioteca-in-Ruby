d58f2c404e0326a253705d0049a79e283c43d1f2
Amandeep Singh <Amandees@thoughtworks.com>
2015-12-08 15:27:45 +0530

    [Release 1, Checkout Book] Added method checkout_book in library to checkout a book and delete that if present from available books

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -1,19 +1,41 @@
 require './src/library'
 
 describe "Library" do
-  
+
+  let(:book1) { double("Book") }
+  let(:book2) { double("Book") }
+  let(:library) { Library.new([book1, book2]) }
+
   describe "#list_books" do
 
     it 'should print headers and list all the books inside library' do
-      book1 = double("Book")
-      book2 = double("Book")
-      library = Library.new([book1, book2])
       expected_headers = "Available books are : \n" + "%-50s %-50s" % ["Name", "Author"] + "Year of Publication\n" + "-"*120 + "\n"
 
       allow(book1).to receive("to_str").and_return "book1"
       allow(book2).to receive("to_str").and_return "book2"
 
       expect(library.list_books).to eq(expected_headers + "book1book2")
+    end
+  end
+  
+  describe '#check_out_book' do
+
+    it 'should not delete book from available_books if book name passed is nil' do
+
+      library.checkout_book nil
+    end
+
+    it 'should not delete book from available_books if book name does not matches' do
+      expect(book1).to receive("name?").with('some random book').and_return(false)
+      expect(book2).to receive("name?").with('some random book').and_return(false)
+
+      library.checkout_book 'some random book'
+    end
+
+    it 'should delete book from available_books if book name matches' do
+      expect(book1).to receive("name?").with('some random book').and_return(true)
+
+      library.checkout_book 'some random book'
     end
   end
 end
--- src/library.rb
+++ src/library.rb
@@ -10,6 +10,13 @@ class Library
     print_headers + @available_books.each{ |book| book.to_str }.join
   end
 
+  def checkout_book that_book_name
+    return if that_book_name.nil?
+    @available_books.each { |book|
+      @available_books.delete book and break if book.name? that_book_name
+    }
+  end
+
   private
   def print_headers
     SyntaxSugar::CONSTANTS::LIST_BOOKS_HEADERS
